5acbe4c59d591786c17f0acf3eedb2991b8b78e4
長谷川裕一 <hase@starlight-storm.com>
2018-06-13 23:38:41 +0900

    @GetMappingの変更。

--- spring-warehouse-answer/src/main/java/com/example/common/web/MenuController.java
+++ spring-warehouse-answer/src/main/java/com/example/common/web/MenuController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MenuController {
-	@GetMapping
+	@GetMapping("/")
 	public String home() {
 		return "main_menu";
 	}
